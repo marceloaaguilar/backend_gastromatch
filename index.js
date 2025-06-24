1d3d9d1bc475f8742dfd806f396c058e4433bebd
Marcelo D'almeida <marcelodalmeida42@gmail.com>
2025-06-24 20:55:10 -0300

    new fix

--- index.js
+++ index.js
@@ -19,8 +19,7 @@ app.use(cookieParser());
 
 app.use(express.json());
 
-// app.use(cors({origin: ['http://127.0.0.1:5173', process.env.VITE_API_URL], credentials: true,}));
-app.use(cors({ origin: '*' }));
+app.use(cors({origin: ['http://127.0.0.1:5173', 'http://localhost:5173', process.env.VITE_API_URL], credentials: true,}));
 
 app.use("/api/users", userRoutes);
 app.use("/api/chefs", chefRoutes);
